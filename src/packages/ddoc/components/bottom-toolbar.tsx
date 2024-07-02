063fcd89f9d82d008dc9165a1f5913d9110b085b
Kyle Nguyen <89263955+kylengn@users.noreply.github.com>
2024-07-02 17:03:41 +0700

    feat: update editor functionalites, bugfixes (#16)

--- src/packages/ddoc/components/bottom-toolbar.tsx
+++ src/packages/ddoc/components/bottom-toolbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
   TextColorPicker,
   TextFormatingPopup,
@@ -10,6 +10,7 @@ import { Drawer, DrawerTrigger } from '../common/drawer';
 import DynamicModal from './dynamic-modal';
 import { TextField } from '../common/textfield';
 import cn from 'classnames';
+import { useMediaQuery } from 'usehooks-ts';
 
 const BottomToolbar = ({
   editor,
@@ -22,11 +23,11 @@ const BottomToolbar = ({
     editor: editor,
     uploadToIpfs,
   });
+  const isMobile = useMediaQuery('(max-width: 1023px)');
 
   const [url, setUrl] = useState('');
   const [linkText, setLinkText] = useState('');
   const [isUrlValid, setIsUrlValid] = useState(true);
-
   const saveLink = () => {
     // cancelled
     if (url === null) {
@@ -61,7 +62,7 @@ const BottomToolbar = ({
           .setLink({ href: finalUrl })
           .command(({ tr }) => {
             tr.insertText(linkText);
-            return true;
+            return true
           })
           .run();
       }
@@ -94,6 +95,52 @@ const BottomToolbar = ({
     const text = editor.state.doc.textBetween(from, to);
     return text;
   };
+  const textFormattingButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    let touchStartTime = 0;
+
+    const handleTouchStart = () => {
+      touchStartTime = Date.now();
+    };
+
+    const handleTouchEnd = () => {
+      const touchEndTime = Date.now();
+      if (touchEndTime - touchStartTime > 500) { // Long press, likely selecting text
+        const { selection } = editor.state;
+        const isTextSelected = selection.from !== selection.to;
+        if (isTextSelected) {
+          textFormattingButtonRef.current?.click();
+        }
+      }
+    };
+
+    const handleMouseUp = () => {
+      if (!isMobile) return;
+      const { selection } = editor.state;
+      const isTextSelected = selection.from !== selection.to;
+      if (isTextSelected) {
+        textFormattingButtonRef.current?.click();
+      }
+    };
+
+    if (isMobile) {
+      document.addEventListener('touchstart', handleTouchStart);
+      document.addEventListener('touchend', handleTouchEnd);
+    } else {
+      document.addEventListener('mouseup', handleMouseUp);
+    }
+
+    return () => {
+      if (isMobile) {
+        document.removeEventListener('touchstart', handleTouchStart);
+        document.removeEventListener('touchend', handleTouchEnd);
+      } else {
+        document.removeEventListener('mouseup', handleMouseUp);
+      }
+    };
+  }, [editor, isMobile]);
+
 
   return (
     <Drawer>
@@ -102,8 +149,11 @@ const BottomToolbar = ({
           if (tool) {
             return (
               <div key={tool.title} className="flex items-center">
-                {tool.title === 'Text formating' ||
-                tool.title === 'Text color' ? (
+                {tool.title === 'Text formating' ? (
+                  <DrawerTrigger asChild>
+                    <button ref={textFormattingButtonRef} onClick={() => tool.onClick()}>{tool.icon}</button>
+                  </DrawerTrigger>
+                ) : tool.title === 'Text color' ? (
                   <DrawerTrigger asChild>
                     <button onClick={() => tool.onClick()}>{tool.icon}</button>
                   </DrawerTrigger>
--- src/packages/ddoc/ddoc-editor.tsx
+++ src/packages/ddoc/ddoc-editor.tsx
@@ -9,7 +9,9 @@ import './styles/editor.scss';
 import 'tippy.js/animations/shift-toward-subtle.css';
 import { useDdocEditor } from './use-ddoc-editor';
 import BottomToolbar from './components/bottom-toolbar';
-import { forwardRef, useImperativeHandle } from 'react';
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import cn from 'classnames';
 
 const DdocEditor = forwardRef(
   (
@@ -29,9 +31,12 @@ const DdocEditor = forwardRef(
     }: DdocProps,
     ref
   ) => {
+    // const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+    const [isTyping, setIsTyping] = useState(false);
+    const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
     const {
       editor,
-      focusEditor,
+      // focusEditor,
       ref: editorRef,
       loading,
       ydoc
@@ -56,6 +61,62 @@ const DdocEditor = forwardRef(
       [editor, ydoc]
     );
 
+
+    // useEffect(() => {
+    //   const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
+
+    //   if (!iOS) {
+    //     return;
+    //   }
+
+    //   const handleFocus = () => {
+    //     setIsKeyboardVisible(true);
+    //   };
+
+    //   const handleBlur = () => {
+    //     setIsKeyboardVisible(false);
+    //   };
+
+    //   editor?.on('focus', handleFocus);
+    //   editor?.on('blur', handleBlur);
+
+    //   return () => {
+    //     editor?.off('focus', handleFocus);
+    //     editor?.off('blur', handleBlur);
+    //   };
+    // }, [editor]);
+
+    useEffect(() => {
+      if (!editor) return;
+
+      const isCharacterKey = (event: KeyboardEvent) => {
+        const { key } = event;
+        return key.length === 1;
+      };
+
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (isCharacterKey(event)) {
+          if (typingTimeoutRef.current) {
+            clearTimeout(typingTimeoutRef.current);
+          }
+          setIsTyping(true);
+          typingTimeoutRef.current = setTimeout(() => {
+            setIsTyping(false);
+          }, 500);
+        }
+      };
+
+      const editorElement = editor.view.dom;
+      editorElement.addEventListener('keydown', handleKeyDown);
+
+      return () => {
+        editorElement.removeEventListener('keydown', handleKeyDown);
+        if (typingTimeoutRef.current) {
+          clearTimeout(typingTimeoutRef.current);
+        }
+      };
+    }, [editor]);
+
     if (!editor || loading) {
       return (
         <div className='w-screen h-screen flex flex-col gap-4 justify-center items-center'>
@@ -68,64 +129,78 @@ const DdocEditor = forwardRef(
     return (
       <div
         data-cy='single-webpage'
-        className='bg-[#f8f9fa]'
+        className='bg-[#f8f9fa] h-full w-full overflow-hidden no-scrollbar'
       >
-        <div className='h-full flex flex-col overflow-scroll no-scrollbar'>
-          <div className='w-full h-full'>
-            <div className='flex items-center w-full h-16 fixed z-10 px-4 bg-[#f8f9fa]'>
-              <div className='flex items-center justify-between gap-2 w-full'>
-                <div className='grow'>
-                  {renderToolLeftSection?.({ editor: editor.getJSON() })}
-                </div>
+        <div className='flex flex-col justify-between items-center'>
+          <div className='flex items-center w-full h-16 fixed z-10 px-4 bg-[#f8f9fa]'>
+            <div className='flex items-center justify-between gap-2 w-full'>
+              <div className='grow'>
+                {renderToolLeftSection?.({ editor: editor.getJSON() })}
+              </div>
 
-                {!isPreviewMode && (
-                  <div className='grow relative hidden xl:block'>
-                    <EditorToolBar
-                      uploadToIpfs={handleImageUploadToIpfs}
-                      editor={editor}
-                    />
-                  </div>
-                )}
-                {renderToolRightSection?.({ editor: editor.getJSON() })}
+              {!isPreviewMode && (
+                <div className='grow relative hidden xl:block'>
+                  <EditorToolBar
+                    uploadToIpfs={handleImageUploadToIpfs}
+                    editor={editor}
+                  />
+                </div>
+              )}
+              {renderToolRightSection?.({ editor: editor.getJSON() })}
+            </div>
+          </div>
+
+          <main className='rounded-[8px] flex flex-col justify-start items-center gap-2 '>
+            <div
+              // onClick={focusEditor}
+              className='mt-8 lg:mt-[5rem] w-screen flex justify-center relative'
+            >
+              <div className='px-4 pt-12 sm:p-[88px] h-screen bg-white  w-full sm:w-[70%] max-w-[856px]'>
+                <div
+                  ref={editorRef}
+                  className='w-full h-full overflow-y-scroll overflow-x-hidden no-scrollbar'
+                >
+                  {!isPreviewMode && (
+                    <div>
+                      <EditorBubbleMenu editor={editor} />
+                      <ColumnsMenu
+                        editor={editor}
+                        appendTo={editorRef}
+                      />
+                    </div>
+                  )}
+                  <EditingProvider isPreviewMode={isPreviewMode}>
+                    <EditorContent editor={editor} />
+                  </EditingProvider>
+                </div>
               </div>
             </div>
+          </main>
 
-            <main className='h-screen lg:h-full w-full rounded-[8px] flex flex-col justify-start items-center gap-2'>
-              <div
-                onClick={focusEditor}
-                className='mt-8 lg:mt-[5rem] w-screen flex justify-center relative'
-              >
-                <div className='px-4 pt-12 sm:p-[88px] h-screen bg-white overflow-scroll no-scrollbar w-full sm:w-[70%] max-w-[856px]'>
-                  <div
-                    ref={editorRef}
-                    className='w-full h-full'
-                  >
-                    {!isPreviewMode && (
-                      <div>
-                        <EditorBubbleMenu editor={editor} />
-                        <ColumnsMenu
-                          editor={editor}
-                          appendTo={editorRef}
-                        />
-                      </div>
-                    )}
-                    <EditingProvider isPreviewMode={isPreviewMode}>
-                      <EditorContent editor={editor} />
-                    </EditingProvider>
-                  </div>
-                </div>
-              </div>
-            </main>
-
-            {!isPreviewMode && (
-              <div className='flex xl:hidden items-center w-full h-16 fixed bottom-0 z-10 px-4 bg-[#f8f9fa]'>
-                <BottomToolbar
-                  uploadToIpfs={handleImageUploadToIpfs}
-                  editor={editor}
-                />
-              </div>
-            )}
-          </div>
+          {!isPreviewMode && (
+            <AnimatePresence>
+              {!isTyping && (
+                <motion.div
+                  initial={{ y: 50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: 50, opacity: 0 }}
+                  transition={{ duration: 0.2 }}
+                  className={cn(
+                    'flex xl:hidden items-center w-full h-16 fixed z-10 px-4 bg-[#f8f9fa] bottom-0',
+                    // {
+                    //   'bottom-[268px]': isKeyboardVisible,
+                    //   'bottom-0': !isKeyboardVisible,
+                    // }
+                  )}
+                >
+                  <BottomToolbar
+                    uploadToIpfs={handleImageUploadToIpfs}
+                    editor={editor}
+                  />
+                </motion.div>
+              )}
+            </AnimatePresence>
+          )}
         </div>
       </div>
     );
