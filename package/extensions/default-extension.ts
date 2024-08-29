2cfbae2dbf7eb2b9474f192ecf19653ef70e2b87
mbj36 <mohitbajo36@gmail.com>
2024-08-29 18:15:59 +0530

    2.0.2-patch-2

--- package/extensions/default-extension.ts
+++ package/extensions/default-extension.ts
@@ -115,7 +115,7 @@ export const defaultExtensions = [
         'color-text-link font-bold transition-colors cursor-pointer select-text pointer-events-auto',
       rel: 'noopener noreferrer',
     },
-    validate: href => /^https?:\/\//.test(href),
+    validate: (href) => /^https?:\/\//.test(href),
     openOnClick: true,
     autolink: true,
   }),
@@ -172,14 +172,14 @@ export const defaultExtensions = [
   ColumnExtension,
   MarkdownPasteHandler,
   Markdown.configure({
-    html: false, // Allow HTML input/output
+    html: true, // Allow HTML input/output
     tightLists: true, // No <p> inside <li> in markdown output
     tightListClass: 'tight', // Add class to <ul> allowing you to remove <p> margins when tight
     bulletListMarker: '-', // <li> prefix in markdown output
     linkify: true, // Create links from "https://..." text
     breaks: true, // New lines (\n) in markdown input are converted to <br>
     transformPastedText: true, // Allow to paste markdown text in the editor
-    transformCopiedText: true, // Copied text is transformed to markdown
+    // transformCopiedText: true, // Copied text is transformed to markdown
   }),
   CharacterCount,
   MathExtension.configure({
