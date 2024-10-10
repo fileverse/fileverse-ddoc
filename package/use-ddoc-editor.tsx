785e3d76f4124cf83b51ebc5ac3c4d2cea288cd5
Mohit kumar Bajoria <mohitbajo36@gmail.com>
2024-10-10 12:52:18 +0530

    Revert "Added smiley-replacer (#106)"
    This reverts commit 835c945bcbb3452b1c43f649b2bc4aafafc947df.

--- package/use-ddoc-editor.tsx
+++ package/use-ddoc-editor.tsx
@@ -13,7 +13,7 @@ import { getAddressName, getTrimmedName } from './utils/getAddressName';
 import { EditorView } from '@tiptap/pm/view';
 import SlashCommand from './components/slash-comand';
 import { EditorState } from '@tiptap/pm/state';
-import customTextInputRules from './extensions/customTextInputRules';
+import superscriptOrdinal from './extensions/superscriptOrdinal';
 
 const usercolors = [
   '#30bced',
@@ -46,7 +46,7 @@ export const useDdocEditor = ({
   const [extensions, setExtensions] = useState([
     ...(defaultExtensions((error: string) => onError?.(error)) as AnyExtension[]),
     SlashCommand((error: string) => onError?.(error)),
-    customTextInputRules,
+    superscriptOrdinal,
   ]);
   const initialContentSetRef = useRef(false);
   const [isContentLoading, setIsContentLoading] = useState(true);
