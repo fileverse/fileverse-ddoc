eaf71a71c7c8d0731f6ad486a1461594e898fd62
Srishti Lodhi <110035350+SrishtiLodhi@users.noreply.github.com>
2024-10-22 18:58:14 +0530

    Changes in UI for hover on PageBreak

--- package/extensions/page-break/page-break-node-view.tsx
+++ package/extensions/page-break/page-break-node-view.tsx
@@ -15,15 +15,34 @@ export const PageBreakNodeView: React.FC<NodeViewProps> = ({
   return (
     <NodeViewWrapper
       as="div"
-      className={cn('flex opacity-0 hover:opacity-100 transition-opacity duration-200 w-full h-full justify-center items-center')}
+      className={cn(
+        'flex relative opacity-0 hover:opacity-100 transition-opacity duration-200 w-full h-full justify-center items-center',
+      )}
     >
-        <Tooltip sideOffset={5} position="bottom" text="Remove page breaker">
-          <LucideIcon
-            name="PageBreakRemove"
-            onClick={handleDeleteNode}
-            className="text-[#77818A] cursor-pointer"
-          />
-        </Tooltip>
+      <div className="absolute top-[-15px] left-0 right-0 h-4">
+        <div
+          className="absolute inset-0 border-b-1 border-transparent"
+          style={{
+            background: 'linear-gradient(to bottom, transparent, #FFDF0A33)',
+          }}
+        ></div>
+      </div>
+
+      <Tooltip sideOffset={5} position="bottom" text="Remove page breaker">
+        <LucideIcon
+          name="PageBreakRemove"
+          onClick={handleDeleteNode}
+          className="text-[#77818A] cursor-pointer"
+        />
+      </Tooltip>
+      <div className="absolute bottom-[-15px] left-0 right-0 h-4">
+        <div
+          className="absolute inset-0 border-b-1 border-transparent"
+          style={{
+            background: 'linear-gradient(to bottom, #FFDF0A33, transparent)',
+          }}
+        ></div>
+      </div>
     </NodeViewWrapper>
   );
 };
