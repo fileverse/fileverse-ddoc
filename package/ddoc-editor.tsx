95f71a002436501599a6ea342f18e148cd762d64
Srishti Lodhi <110035350+SrishtiLodhi@users.noreply.github.com>
2024-11-04 19:42:02 +0530

    Changed margins

--- package/ddoc-editor.tsx
+++ package/ddoc-editor.tsx
@@ -273,7 +273,7 @@ const DdocEditor = forwardRef(
             ref={editorRef}
             className={cn(
               'w-full h-full pt-8 md:pt-0',
-              { '!mt-16': isNativeMobile},
+              { '!mt-20': isNativeMobile},
             )}            
           >
             {!isPreviewMode && (
