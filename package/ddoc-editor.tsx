0c1b53e0384d6f990e1af3c9c08b703f3eb69e95
Kyle Nguyen <kyle.nguyen304@gmail.com>
2024-12-10 15:09:38 +0700

    fix: navbar zIndex

--- package/ddoc-editor.tsx
+++ package/ddoc-editor.tsx
@@ -255,7 +255,7 @@ const DdocEditor = forwardRef(
         <nav
           id="Navbar"
           className={cn(
-            'h-14 bg-[#ffffff] py-2 px-4 flex gap-[40px] items-center justify-between w-screen fixed left-0 top-0 border-b color-border-default z-[60] transition-transform duration-300',
+            'h-14 bg-[#ffffff] py-2 px-4 flex gap-[40px] items-center justify-between w-screen fixed left-0 top-0 border-b color-border-default z-50 transition-transform duration-300',
             {
               'translate-y-0': isNavbarVisible,
               'translate-y-[-100%]': !isNavbarVisible || isPresentationMode,
