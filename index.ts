73cf7639f750eba44d9cb2aa08b34caafe76fc03
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-02-25 11:28:27 +0100

    export headless editor

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { default as DdocEditor } from './package/ddoc-editor';
 export { PreviewDdocEditor } from './package/preview-ddoc-editor';
 export { handleContentPrint } from './package/utils/handle-print';
+export { useHeadlessEditor } from './package/hooks/use-headless-editor';
