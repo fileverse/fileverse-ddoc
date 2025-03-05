39667bbbc381d98c64cb469c70bec197ca276fd7
Joshua Onwuzu <77381596+Joshua-onwuzu@users.noreply.github.com>
2025-03-05 09:52:06 +0100

    Update headless editor (#249)
    * updated headless editor to export util functions
    
    * released 2.1.3-patch-29.2

--- package/hooks/use-headless-editor.tsx
+++ package/hooks/use-headless-editor.tsx
@@ -1,30 +1,29 @@
 import { Editor, JSONContent } from '@tiptap/react';
 import { defaultExtensions } from '../extensions/default-extension';
-import { useState } from 'react';
 import customTextInputRules from '../extensions/customTextInputRules';
 import { PageBreak } from '../extensions/page-break';
 import Collaboration from '@tiptap/extension-collaboration';
 import * as Y from 'yjs';
 import { isJSONString } from '../utils/isJsonString';
-import { toUint8Array } from 'js-base64';
+import { fromUint8Array, toUint8Array } from 'js-base64';
 import { sanitizeContent } from '../utils/sanitize-content';
 
 export const useHeadlessEditor = () => {
-  const [ydoc] = useState(new Y.Doc());
-  const [extensions] = useState([
-    ...defaultExtensions(() => null, ''),
-    customTextInputRules,
-    PageBreak,
-    Collaboration.configure({
-      document: ydoc,
-    }),
-  ]);
-
   const getEditor = () => {
-    return new Editor({
+    const ydoc = new Y.Doc();
+    const extensions = [
+      ...defaultExtensions(() => null, ''),
+      customTextInputRules,
+      PageBreak,
+      Collaboration.configure({
+        document: ydoc,
+      }),
+    ];
+    const editor = new Editor({
       extensions,
       autofocus: false,
     });
+    return { editor, ydoc };
   };
 
   const isContentYjsEncoded = (
@@ -36,7 +35,7 @@ export const useHeadlessEditor = () => {
     );
   };
 
-  const mergeAndApplyUpdate = (contents: string[]) => {
+  const mergeAndApplyUpdate = (contents: string[], ydoc: Y.Doc) => {
     const parsedContents = contents.map((content) => toUint8Array(content));
     Y.applyUpdate(ydoc, Y.mergeUpdates(parsedContents));
   };
@@ -44,12 +43,13 @@ export const useHeadlessEditor = () => {
   const setContent = (
     initialContent: string | string[] | JSONContent,
     editor: Editor,
+    ydoc: Y.Doc,
   ) => {
     if (!editor) throw new Error('cannot set content without editor');
     const isYjsEncoded = isContentYjsEncoded(initialContent as string);
     if (isYjsEncoded) {
       if (Array.isArray(initialContent)) {
-        mergeAndApplyUpdate(initialContent);
+        mergeAndApplyUpdate(initialContent, ydoc);
       } else {
         Y.applyUpdate(ydoc, toUint8Array(initialContent as string));
       }
@@ -62,5 +62,41 @@ export const useHeadlessEditor = () => {
     }
   };
 
-  return { setContent, getEditor };
+  const convertJSONContentToYjsEncodedString = (content: JSONContent) => {
+    const { editor, ydoc } = getEditor();
+    setContent(content, editor, ydoc);
+    return fromUint8Array(Y.encodeStateAsUpdate(ydoc));
+  };
+
+  const downloadContentAsMd = (
+    content: string | string[] | JSONContent,
+    title: string,
+  ) => {
+    const { editor, ydoc } = getEditor();
+    setContent(content, editor, ydoc);
+    if (editor) {
+      const generateDownloadUrl = editor.commands.exportMarkdownFile();
+      if (generateDownloadUrl) {
+        const url = generateDownloadUrl;
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = title + '.md';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+      } else {
+        throw new Error('Failed to generate download url');
+      }
+    } else {
+      throw new Error('Editor is not available');
+    }
+  };
+
+  return {
+    setContent,
+    getEditor,
+    convertJSONContentToYjsEncodedString,
+    downloadContentAsMd,
+  };
 };
