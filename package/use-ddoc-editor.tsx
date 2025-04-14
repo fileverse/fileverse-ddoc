c21259072e9f0612c5e88271456d6670cf764cc7
Mohit kumar Bajoria <mohitbajo36@gmail.com>
2025-04-14 13:54:53 +0530

    fix: loading editor and remove scroll position code (#284)

--- package/use-ddoc-editor.tsx
+++ package/use-ddoc-editor.tsx
@@ -53,7 +53,6 @@ export const useDdocEditor = ({
   setCharacterCount,
   setWordCount,
   secureImageUploadUrl,
-  scrollPosition,
   ddocId,
   enableIndexeddbSync,
   unFocused,
@@ -495,25 +494,6 @@ export const useDdocEditor = ({
 
       initialContentSetRef.current = true;
     }
-
-    const scrollTimeoutId = setTimeout(() => {
-      if (ref.current && !!scrollPosition && editor) {
-        const coords = editor.view.coordsAtPos(scrollPosition);
-        const editorContainer = ref.current;
-        editorContainer.scrollTo({
-          top: editorContainer.scrollTop + coords.top - 500,
-          behavior: 'smooth',
-        });
-      }
-      initialContentSetRef.current = false;
-      if (editor && initialContent === undefined) {
-        setIsContentLoading(false);
-      }
-    });
-
-    return () => {
-      clearTimeout(scrollTimeoutId);
-    };
   }, [initialContent, editor, ydoc]);
 
   const startCollaboration = async () => {
