fbdfa0db26fe65c341c56a1eb233c7d131e9c597
Joshua Onwuzu <77381596+Joshua-onwuzu@users.noreply.github.com>
2025-04-16 11:06:25 +0100

    add animation feedbacks and loading screen for viewer page (#291)
    * add animation feedbacks and loading screen for viewer page
    
    * moved preview content loader to new file

--- package/components/motion-div.tsx
+++ package/components/motion-div.tsx
@@ -19,3 +19,24 @@ export const fadeInTransition = (element: JSX.Element, key: string) => {
     </motion.div>
   );
 };
+
+export const slideUpVariants = {
+  initial: { y: 10, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  exit: { y: -10, opacity: 0 },
+};
+
+export const slideUpTransition = (element: JSX.Element, key: string) => {
+  return (
+    <motion.div
+      key={key}
+      variants={slideUpVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.2 }}
+    >
+      {element}
+    </motion.div>
+  );
+};
