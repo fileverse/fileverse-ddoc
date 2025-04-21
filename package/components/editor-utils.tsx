ea59e2f5a805d446668bb4256df4806c4f8d4a9d
Maitra Khatri <54719132+maitrakhatri@users.noreply.github.com>
2025-04-21 17:17:16 +0530

    make floating toolbar disappear and add link with enter key (#294)

--- package/components/editor-utils.tsx
+++ package/components/editor-utils.tsx
@@ -1043,6 +1043,12 @@ export const LinkPopup = ({
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           setUrl(e.target.value)
         }
+        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            apply();
+          }
+        }}
         className="w-full"
         placeholder="Add link here"
         value={url}
@@ -1053,7 +1059,10 @@ export const LinkPopup = ({
         </Button>
         <Button
           variant="secondary"
-          onClick={() => editor.chain().focus().unsetLink().run()}
+          onClick={() => {
+            editor.chain().focus().unsetLink().run();
+            setToolVisibility(IEditorTool.NONE);
+          }}
           className="min-w-fit h-[36px]"
         >
           Unset
