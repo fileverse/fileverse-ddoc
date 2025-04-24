44ecae463d6c326830d0468cff5a44e5246522e2
Kyle Nguyen <kyle.nguyen304@gmail.com>
2025-04-24 20:32:53 +0700

    revert

--- demo/src/vite-env.d.ts
+++ demo/src/vite-env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module 'virtual:serwist' {
-  interface ExtendedServiceWorkerRegistration extends ServiceWorkerRegistration {
-    register(): Promise<void>;
-  }
-  export function getSerwist(): Promise<ExtendedServiceWorkerRegistration | undefined>;
-}
