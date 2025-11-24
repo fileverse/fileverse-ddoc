b9d4029d4c6c30a04280e661f4859f564b4f4014
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-11-24 17:41:16 +0530

    markdown extension

--- package/extensions/default-extension.ts
+++ package/extensions/default-extension.ts
@@ -23,7 +23,7 @@ import { TrailingNode } from './trailing-node';
 import { NodeType } from '@tiptap/pm/model';
 import { InputRule } from '@tiptap/core';
 import { actionButton } from './action-button';
-import { Markdown } from '@tiptap/markdown';
+import { Markdown } from 'tiptap-markdown';
 import Typography from '@tiptap/extension-typography';
 import MarkdownPasteHandler from './mardown-paste-handler';
 import HtmlExportExtension from './html-export';
@@ -237,16 +237,14 @@ export const defaultExtensions = ({
   HtmlExportExtension(ipfsImageFetchFn, fetchV1ImageFn),
   TextExportExtension(),
   Markdown.configure({
-    markedOptions: {
-      //tightListClass: 'tight', // Add class to <ul> allowing you to remove <p> margins when tight
-      //bulletListMarker: '-', // <li> prefix in markdown output
-      //linkify: true, // Create links from "https://..." text
-      breaks: true, // New lines (\n) in markdown input are converted to <br>
-      // transformPastedText: true, // Allow to paste markdown text in the editor
-    },
-    // tightLists: true, No <p> inside <li> in markdown output
-    // html: true,  Allow HTML input/output
-    // transformCopiedText: true, // Copied text is transformed to markdown
+    tightListClass: 'tight',
+    bulletListMarker: '-',
+    linkify: true,
+    breaks: true,
+    transformPastedText: true,
+    tightLists: true,
+    html: true,
+    transformCopiedText: true,
   }),
   CharacterCount,
   MathExtension.configure({
