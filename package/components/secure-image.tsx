eb3210e27aaeb4b82540ccf807d65a3b2db0bd7c
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-11-25 15:17:18 +0530

    fix: privateKey

--- package/components/secure-image.tsx
+++ package/components/secure-image.tsx
@@ -46,8 +46,7 @@ export const SecureImage = forwardRef<HTMLImageElement, Props>(
 
             const decryptedArrayBuffer = await decryptImage({
               encryptedKey,
-              // @ts-expect-error Bhavesh to fix
-              privateKey: toByteArray(privateKey),
+              privateKey: new Uint8Array(toByteArray(privateKey)).buffer,
               iv,
               imageBuffer,
             });
