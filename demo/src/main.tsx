a2e73df03b1ebc3421bf2050f114fc4c23ffdadb
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2026-02-13 12:24:50 +0530

    installed react scan

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,8 +1,13 @@
+// src/index
+import { scan } from 'react-scan'; // must be imported before React and React DOM
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from '@fileverse/ui';
-
+scan({
+  enabled: true,
+});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
