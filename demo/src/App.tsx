b0bcb24a4e93b8c5af17840b4d3206fdb52c0635
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2026-02-20 06:25:48 +0100

    fix minor

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -478,6 +478,7 @@ function App() {
         collabConfig={collabConfig}
         onCollaboratorChange={onCollaboratorChange}
         documentStyling={documentStyling}
+        isDDocOwner={true}
       />
       <Toaster
         position={!isMobile ? 'bottom-right' : 'center-top'}
