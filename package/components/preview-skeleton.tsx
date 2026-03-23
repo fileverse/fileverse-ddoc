b4a496c58bb7692969b9415c829132bd3f0faa91
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2026-03-23 12:00:21 +0530

    fix: update dark gradient colors in PreviewSkeleton component

--- package/components/preview-skeleton.tsx
+++ package/components/preview-skeleton.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@fileverse/ui';
 
 const className =
-  'h-6 bg-gradient-to-r [--from:210,_17%,_98%,_1] [--to:0,_0%,_93%,_1] dark:[--from:210,_17%,_9%,_1] dark:[--to:0,_0%,_9%,_1] from-[hsl(var(--from))] to-[hsl(var(--to))]';
+  'h-6 bg-gradient-to-r [--from:210,_17%,_98%,_1] [--to:0,_0%,_93%,_1] dark:[--from:0,0%,16%,1] dark:[--to:0,0%,12%,1] from-[hsl(var(--from))] to-[hsl(var(--to))]';
 
 const PreviewSkeleton = ({ className: classStyle }: { className?: string }) => {
   return <div className={cn(className, classStyle)}></div>;
